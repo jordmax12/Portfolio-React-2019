200e58873ba057718fcc4b9b3477695efc33de55
jordmax12 <jordmax12@gmail.com>
2019-05-24 21:08:42 -0500

    removed margin from body

--- views/wrapper.js
+++ views/wrapper.js
@@ -5,7 +5,9 @@ const wrapper = `<html>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
-
+    <style>
+        body { margin: 0 !important; }
+    </style>
 </head>
 <body>
     <div id="app"></div>      
