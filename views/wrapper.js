2e5f795d9d2ab0ef6aaba7a4691ccd932a5df54b
jordmax12 <jordmax12@gmail.com>
2019-05-27 21:13:18 -0500

    mobile fix

--- views/wrapper.js
+++ views/wrapper.js
@@ -1,6 +1,8 @@
 const wrapper = `<html>
 <head>
     <meta charSet="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Jordan Max</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
